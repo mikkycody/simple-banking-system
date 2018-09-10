c4f950a758df93177e5f77390dfe8daaff06177d
Samuel George <samuegeorge58@gmail.com>
2018-09-10 07:12:39 +0100

    navbar fixes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,10 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!---->
+    <link href="{{{ asset('img/bank-512.png') }}}" rel="shortcut icon">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'YingBanking') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -18,13 +20,22 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Styles -->
+        <style>
+            .carousel-caption {
+            position: absolute;
+            top:30%;
+            }
+        </style>
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+
+                <img style = "float:left;" class = "img img-responsive" width = "6%" alt="logo" src="{{ asset('img/bank-512.png') }}"/>
+                <a class="navbar-brand" style="padding-top:2%;" href="{{ url('/') }}">
+                    {{ config('app.name', 'YingBanking') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -47,10 +58,16 @@
                                 <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('transfer') }}">{{ __('Transfer') }}</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="{{ route('pay') }}">{{ __('Bill Pay') }}</a>
+                            </li>
+                            <li class="nav-item">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->firstname }}
+                            </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
