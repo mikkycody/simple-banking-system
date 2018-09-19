eab166659eaa067d4f5edb5c04f6aebcf4ae76a8
Samuel George <samuegeorge58@gmail.com>
2018-09-19 06:53:50 +0100

    setup db

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-01.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', ''),
+            'username' => env('DB_USERNAME', 'b866688840c435'),
+            'password' => env('DB_PASSWORD', '55e0fe36'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
