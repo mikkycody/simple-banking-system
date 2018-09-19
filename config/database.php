68f33a1185c0f0cd8b5060a1bf3e9554b1db20fd
Samuel George <samuegeorge58@gmail.com>
2018-09-19 07:23:36 +0100

    db

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'us-cdbr-iron-east-01.cleardb.net'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'heroku_c0a32514739800d'),
-            'username' => env('DB_USERNAME', 'b866688840c435'),
-            'password' => env('DB_PASSWORD', '55e0fe36'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
